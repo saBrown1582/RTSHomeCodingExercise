1aab09f9041e400ec1557f9cb70041fda54eb9e5
saBrown1582 <94553575+saBrown1582@users.noreply.github.com>
2021-11-17 11:19:44 -0500

    Add files via upload

---
+++ RTS Coding Exercise/RTS Coding Exercise/HomeCodingExercise.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class HomeCodingExercise
+{
+    public Dictionary<string, int> aboveBelow(List<int> list, int comparisonValue)
+    {
+        var returnValue = new Dictionary<string, int>();
+
+        returnValue.Add("above", list.Where(i => i > comparisonValue).Count());
+        returnValue.Add("below", list.Where(i => i < comparisonValue).Count());
+
+        return returnValue;
+    }
+
+    public string stringRotation(string original, int rotationAmount)
+    {
+        if (rotationAmount < 0) throw new ArgumentException("rotationAmount must be positive");
+        string returnValue;
+
+        int modRotationAmount = rotationAmount % original.Length;
+        int overflowCutoffIndex = original.Length - modRotationAmount;
+
+        returnValue = original.Substring(overflowCutoffIndex, modRotationAmount) + original.Substring(0, overflowCutoffIndex);
+
+        return returnValue;
+    }
+}
